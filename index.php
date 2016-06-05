b72efc9c55e498237da0f63adf29206563235543
chenghuajie <649781645@qq.com>
2016-06-05 19:04:29 +0800

    项目入口文件

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
 //入口文件
+
+//PHP版本检查
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG', true);
 
@@ -10,8 +14,13 @@ define('APP_PATH', './Application/');
 //定义运行缓存等目录
 define('RUNTIME_PATH', './RunTime/');
 
-//阻止自动生成安全文件
+//是否自动生成安全文件index.php
 define('DIR_SECURE_FILENAME', false);
+
+if(!is_file(APP_PATH . 'User/Conf/config.php')){
+  header('Location: ./install.php');
+  exit;
+}
 
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
